1c8c32ec75cdbcf310d3401da0406cdcc9ec261f
saniz7 <sanizsanij@gmail.com>
2024-06-19 08:34:07 +0545

    chores

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,6 @@
-import http from './httpService';
-import jwtDecode from 'jwt-decode';
-import httpService from './httpService';
+import http from "./httpService";
+import jwtDecode from "jwt-decode";
+import httpService from "./httpService";
 const apiLogin = "/users/login";
 const apiLogout = "/logout";
 const tokenKey = "token";
@@ -8,52 +8,65 @@ const roleKey = "orgName";
 const userid = "userid";
 
 export async function login(username, password, org) {
-    const {data : res} = await http.post(apiLogin, { username, password, orgName: org });
+  const { data: res } = await http.post(apiLogin, {
+    username,
+    password,
+    orgName: org,
+  });
 
-    localStorage.setItem(tokenKey, res.token);
-    localStorage.setItem(roleKey, res.orgName);
-    localStorage.setItem(userid, res.userId);
-    
-    return res;
+  localStorage.setItem(tokenKey, res.token);
+  localStorage.setItem(roleKey, res.orgName);
+  localStorage.setItem(userid, res.userId);
+
+  return res;
 }
 
 export async function verifyUser() {
-    const token = localStorage.getItem(tokenKey);
-    localStorage.setItem(tokenKey, token);
+  const token = localStorage.getItem(tokenKey);
+  localStorage.setItem(tokenKey, token);
 }
 
 export async function loginWithJwt(jwt) {
-    localStorage.setItem(tokenKey, jwt);
+  localStorage.setItem(tokenKey, jwt);
 }
 
 export async function logout() {
-    // await httpService.get(apiLogout);
-    console.log("logout called");
-    localStorage.clear();
-    return true;
+  // await httpService.get(apiLogout);
+  console.log("logout called");
+  localStorage.clear();
+  return true;
 }
 
 export function getCurrentUser() {
-    try {
-        const jwt = localStorage.getItem(tokenKey);
+  try {
+    const jwt = localStorage.getItem(tokenKey);
 
-        return jwtDecode(jwt).id;
-    } catch (ex) {
-        return null;
-    }
+    return jwtDecode(jwt).id;
+  } catch (ex) {
+    return null;
+  }
 }
 
 export function getJwt() {
-    return localStorage.getItem(tokenKey);
+  return localStorage.getItem(tokenKey);
 }
 
 export function getRole() {
-    return localStorage.getItem(roleKey);
+  return localStorage.getItem(roleKey);
 }
 export function getId() {
-    return localStorage.getItem(userid);
+  return localStorage.getItem(userid);
 }
 
-const authService = { login, loginWithJwt, logout, getCurrentUser, getJwt, verifyUser, getRole, getId };
+const authService = {
+  login,
+  loginWithJwt,
+  logout,
+  getCurrentUser,
+  getJwt,
+  verifyUser,
+  getRole,
+  getId,
+};
 
 export default authService;
