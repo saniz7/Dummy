c9e32f6571998016edd02db460ab24a6a914c50c
saniz7 <sanizsanij@gmail.com>
2024-07-11 08:28:30 +0545

    fix:backend modlule for pharmacy

--- backend/Controller/Chemist/Chemist.js
+++ backend/Controller/Chemist/Chemist.js
@@ -1,99 +1,155 @@
-const User = require('../../Model/Users');
-const constants = require('../../config/constants.json')
-const log4js = require('log4js');
-const logger = log4js.getLogger('BasicNetwork');
-const helper = require('../../app/helper')
-const invoke = require('../../app/invoke')
-const qscc = require('../../app/qscc')
-const query = require('../../app/query')
-const jwt = require('jsonwebtoken');
-const Record = require('../../Model/PaitientRecords');
-logger.level = 'debug';
+const User = require("../../Model/Users");
+const constants = require("../../config/constants.json");
+const log4js = require("log4js");
+const logger = log4js.getLogger("BasicNetwork");
+const helper = require("../../app/helper");
+const invoke = require("../../app/invoke");
+const qscc = require("../../app/qscc");
+const query = require("../../app/query");
+const jwt = require("jsonwebtoken");
+const Record = require("../../Model/PaitientRecords");
+logger.level = "debug";
 function getErrorMessage(field) {
-    var response = {
-        success: false,
-        message: field + ' field is missing or Invalid in the request'
-    };
-    return response;
+  var response = {
+    success: false,
+    message: field + " field is missing or Invalid in the request",
+  };
+  return response;
 }
 
-exports.medicineRecord = async (req,res,next)=>{
+exports.medicineRecord = async (req, res, next) => {
+  const patientId = req.body.patientId;
+  const recordId = req.body.recordId;
+  const medicineBill = req.body.medicineBill;
+  const medicines = req.body.medicines;
+  // const username = req.body.username;
+  const orgName = "pharmacy";
+  const transient = {};
 
-    const patientId = req.body.patientId;
-    const recordId = req.body.recordId;
-    const medicineBill = req.body.medicineBill;
-    const medicines = req.body.medicines;
-    // const username = req.body.username;
-    const orgName = "pharmacy";
-    const transient = {};
-    const args = [ recordId, medicineBill, medicines];
-
-    // MongoDB access code to check if pharmacy has access to this recordId
-    var flag = 0;
-    let username;
-    const checkAccess = await User.findOne({userId:req.session.uid}).then((result)=>{
-        console.log("medicalRecord 2");
-        console.log("result: ", result);
-        username = result.userName;
-        if(!result.access.includes(patientId)){
-            console.log("medicalRecord 3");
-            flag = 1;
-            return res.status(400).json({ success: false, message: `You do not have right to read the prescription for this user` });
-        }
-    });
-
-    if(flag == 1){
-        return;
+  // MongoDB access code to check if pharmacy has access to this recordId
+  var flag = 0;
+  let username;
+  var neww;
+  const checkAccess = await User.findOne({ userId: req.session.uid }).then(
+    (result) => {
+      console.log("medicalRecord 2");
+      console.log("result: ", result);
+      username = result.userName;
+      if (!result.access.includes(patientId)) {
+        console.log("medicalRecord 3");
+        flag = 1;
+        return res.status(400).json({
+          success: false,
+          message: `You do not have right to read the prescription for this user`,
+        });
+      }
     }
-    
+  );
 
-    // invoke code to update the medicalRecord with recordId
+  if (flag == 1) {
+    return;
+  }
+  let existingRecord;
+  try {
+    existingRecord = await query.query(
+      "main-channel1",
+      "chaincode1",
+      [recordId],
+      "getMedicineData",
+      username,
+      orgName
+    );
+    existingRecord.medicines = existingRecord.medicines; // Parse the existing medicines data
+    existingRecord.medicineBill = existingRecord.medicineBill; // Parse the existing medicine bills data
+  } catch (error) {
+    console.log("Record does not exist. Creating a new one.", error);
+    existingRecord = { medicines: [], medicineBill: [] };
+  }
+  console.log(
+    "23",
+    JSON.parse(JSON.parse(JSON.parse(existingRecord.medicines)))
+  );
+  console.log("24", existingRecord.medicineBill);
+  // invoke code to update the medicalRecord with recordId
+  console.log("25", medicineBill);
+  let ex = JSON.parse(JSON.parse(JSON.parse(existingRecord.medicines)));
+  if (existingRecord.medicineBill) {
+    let bi = JSON.parse(JSON.parse(existingRecord.medicineBill));
+    console.log(bi);
+    newbi = [...bi, ...medicineBill];
+  } else {
+    newbi = [...medicineBill];
+  }
+  neww = [...ex, ...medicines];
+  console.log(JSON.stringify(newbi));
+  console.log(JSON.stringify(medicineBill));
+  const args = [
+    recordId,
+    JSON.stringify(newbi),
+    JSON.stringify(neww),
+    username,
+  ];
+  console.log(args);
+  let message = await invoke.invokeTransaction(
+    "main-channel1",
+    "chaincode1",
+    "medicineDispensed",
+    args,
+    username,
+    orgName,
+    transient
+  );
+  console.log("medicalRecord 6");
+  console.log(`message result is : ${message}`);
 
-    let message = await invoke.invokeTransaction("main-channel1", "chaincode1", "medicineDispensed", args, username, orgName, transient);
-    console.log("medicalRecord 6");
-    console.log(`message result is : ${message}`)
+  res.status(200).json({
+    success: true,
+    message: "Medicine Data updated successfully",
+  });
+};
 
-    res.status(200).json({
-        success: true,
-        message: "Medicine Data updated successfully"
-    })
-}
+exports.getMedicineRecord = async (req, res, next) => {
+  const { patientId } = req.query;
 
+  const checkAccess = await User.findOne({ userId: req.session.uid }).then(
+    (result) => {
+      if (!result.access.includes(patientId)) {
+        return res.status(400).json({
+          success: false,
+          message: `You do not have right to read the prescription for this user`,
+        });
+      }
+    }
+  );
 
+  var username;
 
-exports.getMedicineRecord = async (req,res,next)=>{
+  const getPatient = await User.findOne({ userId: patientId }).then(
+    (result) => {
+      console.log("result: ", result, "patientId: ", patientId);
+      username = result.userName;
+    }
+  );
 
-    const {patientId} = req.query;
+  var recordsData = [];
 
-    const checkAccess = await User.findOne({userId:req.session.uid}).then((result)=>{
+  const records = await Record.find({ patientId }).then(async (result) => {
+    await Promise.all(
+      Object.keys(result).map(async (item) => {
+        let message = await query.query(
+          "main-channel1",
+          "chaincode1",
+          [result[0].RecordId],
+          "getMedicineData",
+          username,
+          "patient"
+        );
 
-        if(!result.access.includes(patientId)){
-            return res.status(400).json({ success: false, message: `You do not have right to read the prescription for this user` });
-        }
+        message.medicines = JSON.parse(JSON.parse(message.medicines));
+        recordsData.push(message);
+      })
+    );
+  });
 
-    })
-
-    var username;
-
-    const getPatient = await User.findOne({userId: patientId}).then((result)=>{
-        console.log("result: ", result, "patientId: ", patientId);
-        username = result.userName;
-
-    })
-
-    var recordsData=[]
-
-    const records = await Record.find({patientId}).then(async (result)=>{
-
-        await Promise.all(Object.keys(result).map(async (item)=>{
-        
-            let message = await query.query("main-channel1", "chaincode1", [result[0].RecordId], "getMedicineData", username, "patient");
-
-            message.medicines = JSON.parse(JSON.parse(message.medicines));
-            recordsData.push(message);
-        }));
-    });
-
-    res.status(200).json({ success: true, recordsData });
-
-}
+  res.status(200).json({ success: true, recordsData });
+};
